96c723415c5a9e1b3a72bbffbd8f72af1a92fd97
Zafarbek <zafarbek19980219@gmail.com>
2021-12-26 03:22:22 +0500

    'HasAuthotory
    '

--- app-online-shop-server/src/main/java/uz/teda/appOnlineShop/config/ApplicationSecurityConfig.java
+++ app-online-shop-server/src/main/java/uz/teda/appOnlineShop/config/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package uz.teda.appOnlineShop.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static uz.teda.appOnlineShop.entity.enums.AplicationUserPermession.COURSE_WRITE;
 import static uz.teda.appOnlineShop.entity.enums.ApplicationUserRole.*;
 
 @Configuration
@@ -34,7 +36,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasAnyRole(STUDENT.name())
-                .antMatchers("/management/api/**").hasAnyRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
--- app-online-shop-server/src/main/java/uz/teda/appOnlineShop/controller/StudentManagerController.java
+++ app-online-shop-server/src/main/java/uz/teda/appOnlineShop/controller/StudentManagerController.java
@@ -12,40 +12,47 @@ public class StudentManagerController {
 
 
     private static final List<Student> students = Arrays.asList(
-            new Student(4, "Sardor Bobojonov "),
+            new Student(1, "Jumaniyozov Nizomjon "),
+
             new Student(2, "Odamboy Egamberdiyev"),
             new Student(3, "Azizbek Jumaboyev "),
-            new Student(1, "Jumaniyozov Nizomjon ")
-
+            new Student(4, "Sardor Bobojonov ")
     );
-@GetMapping
+
+    @GetMapping
     public List<Student> getAllStudents() {
-    System.out.println("getAllStudents");
+        System.out.println("getAllStudents");
         return students;
     }
-@GetMapping(path = "/{studentId}")
+
+    @GetMapping(path = "/{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
-    System.out.println("getStudent");
+        System.out.println("getStudent");
         return students.stream().filter(student -> studentId.equals(student.getStudentId()))
                 .findAny()
-                .orElseThrow(()->new IllegalStateException("Student "+ studentId+" doesn't exist" ));
-}
-@PostMapping
+                .orElseThrow(()->new IllegalStateException("Student "+studentId+"doesn't exist"));
+        //    return students.stream().filter(student -> studentId.equals(student.getStudentId()))
+        //            .findAny()
+        //            .orElseThrow(()->new IllegalStateException("Student "+ studentId+" doesn't exist" ));
+    }
+
+    @PostMapping
     public void registrNewStudent(@RequestBody Student student) {
-    System.out.println("registrNewStudent");
+        System.out.println("registrNewStudent");
         System.out.println(student);
 
     }
-@DeleteMapping(path = "{studentId}")
+
+    @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
-    System.out.println("deleteStudent");
+        System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
-    public void updateStudent(@PathVariable("studentId") Integer studentId,@RequestBody Student student) {
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
 
         System.out.println("updateStudent");
-    System.out.println(String.format("%s  %s%n", student,studentId));
+        System.out.println(String.format("%s  %s%n", student, studentId));
     }
 }
